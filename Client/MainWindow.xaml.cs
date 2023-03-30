d6acee7095bb5418f0238551e3205b715805b41c
NikitaB1221 <44338356+NikitaB1221@users.noreply.github.com>
2023-03-31 00:39:22 +0300

    Add files via upload

---
+++ Client/MainWindow.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Client
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void SendButton_Click(object sender, RoutedEventArgs e)
+        {
+            IPEndPoint endpoint;  // копия - как у сервера                                              // На окне IP - это текст ("127.0.0.1")
+            try                                                                                         // Для его перевода в число используется
+            {                                                                                           // IPAddress.Parse
+                IPAddress ip = IPAddress.Parse(serverIp.Text);                                          // Аналогично - порт
+                int port = Convert.ToInt32( serverPort.Text);                                           // парсим число из текста
+                endpoint = new(ip, port);                                                               // 
+            }                                                                                           // endpoint - комбинация IP и порта
+            catch                                                                                       // 
+            {
+                MessageBox.Show("Check server network parameters");
+                return;
+            }                                                                                             
+                                                                                                        // создаем сокет подключения
+            Socket clientSocket = new(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp); // адресация IPv4         
+            byte[] buffer = new byte[1024];
+            StringBuilder sb = new StringBuilder();
+
+            try                                                                                         // Двусторонний сокет (и читать, и писать)
+            {                                                                                           // Протокол сокета - ТСР
+                clientSocket.Connect(endpoint);
+                clientSocket.Send( Encoding.UTF8.GetBytes( messageTextBox.Text));
+
+                do
+                {
+                    int n = clientSocket.Receive(buffer);
+                    sb.Append(System.Text.Encoding.UTF8.GetString(buffer, 0, n));
+                } while (clientSocket.Available > 0);
+                String str = sb.ToString();
+                Dispatcher.Invoke(() =>
+                chatLogs.Text += str + "\n");
+
+                clientSocket.Shutdown(SocketShutdown.Both);
+                clientSocket.Dispose();
+            }
+            catch (Exception ex)
+            {
+                chatLogs.Text += ex.Message + "\n";
+            }
+
+        }
+    }
+}
---
+++ Server/MainWindow.xaml.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace Server
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Socket? listenSocket;  //  Слушающий сокет -- постоянно активен пока сервер вкл
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        IPEndPoint? endpoint;
+        private void StartServer_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                IPAddress ip = IPAddress.Parse(serverIp.Text);
+                int port = Convert.ToInt32(serverPort.Text);
+                endpoint = new(ip, port);
+            }
+            catch
+            {
+                MessageBox.Show("Check start network parameters");
+                throw;
+            }
+            listenSocket = new(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            new Thread(StartServerMethod).Start(endpoint);
+            
+        }
+
+        private void StartServerMethod(object? param)
+        {
+            if (listenSocket is null) return;
+            IPEndPoint? endpoint = param as IPEndPoint;
+            if (endpoint is null) return;
+            Dispatcher.Invoke(() => 
+            {
+                StartServer.IsEnabled = false;
+                StopServer.IsEnabled = true;
+                serverLogs.Text = ""; 
+            });
+            try
+            {
+                listenSocket.Bind(endpoint);
+                listenSocket.Listen(100);
+                Dispatcher.Invoke(() => 
+                { 
+                    serverLogs.Text += serverLogs.Text += "Server started\n";
+                    serverStatus.Content = "ON";
+                    serverStatus.Foreground = Brushes.Green;
+                });
+
+                byte[] buffer = new byte[1024];
+                while (true)
+                {
+                    Socket socket = listenSocket.Accept();
+                    StringBuilder sb = new StringBuilder();
+                    do
+                    {
+                        int n = socket.Receive(buffer);
+                        sb.Append(System.Text.Encoding.UTF8.GetString(buffer, 0, n));
+                    } while (socket.Available > 0);
+                    String str = sb.ToString();
+                    Dispatcher.Invoke(() =>
+                    serverLogs.Text += str + "\n");
+
+                    str = "Received at " + DateTime.Now;
+
+                    socket.Send(Encoding.UTF8.GetBytes(str));
+                    socket.Shutdown(SocketShutdown.Both);
+                    socket.Dispose();
+                }
+            }
+            catch (Exception ex)
+            {
+                Dispatcher.Invoke(() =>
+                {
+                serverLogs.Text += "Server stopped " + ex.Message + $" {DateTime.Now}" +"\n";
+                    serverStatus.Content = "OFF";
+                    serverStatus.Foreground = Brushes.Red;
+                });
+            }
+        }
+        private void StopServer_Click(object sender, RoutedEventArgs e)
+        {
+            Dispatcher.Invoke(() =>
+            {
+                StartServer.IsEnabled = true;
+                StopServer.IsEnabled = false;
+            });
+            listenSocket?.Close();
+        }
+
+        private void Window_Closed(object sender, EventArgs e)
+        {
+            if (listenSocket is null) return;
+            if (listenSocket is not null)
+            {
+                try
+                {
+                    listenSocket?.Close();
+                    this.Close();
+                }
+                catch (Exception ex)
+                {
+                    Dispatcher.Invoke(() => serverLogs.Text += "Server stopped " + ex.Message + $" in {DateTime.Now}" +"\n");
+                }
+            }
+        }
+    }
+}
